8e29f076ef251aed8b84ba7f1ef89e15697b32fd
mohammed ali maaouani <mohammed.m@bmeholding.com>
2024-09-10 13:24:00 +0300

    create new table application_condition_exec

---
+++ tr/trad_ar_application_condition_exec.php
@@ -0,0 +1,18 @@
+<?php
+	$trad["application_condition_exec"]["step1"] = "التعريف";
+
+	$trad["application_condition_exec"]["applicationconditionexec.single"] = "تطبيق شروط خطة التقديم";
+	$trad["application_condition_exec"]["applicationconditionexec.new"] = "جديد ة";
+	$trad["application_condition_exec"]["application_condition_exec"] = "تطبيقات شروط خطط التقديم";
+
+	$trad["application_condition_exec"]["applicant_id"] = "المتقدم";
+	$trad["application_condition_exec"]["application_plan_id"] = "خطة التقديم";
+	$trad["application_condition_exec"]["adesire_id"] = "الرغبة";
+	$trad["application_condition_exec"]["acondition_id"] = "الشرط";
+	$trad["application_condition_exec"]["field_value"] = "قيمة الحقل";
+	$trad["application_condition_exec"]["field_date"] = "تاريخ القيمة";
+	$trad["application_condition_exec"]["condition_exec_date"] = "تاريخ تنفيذ الشرط";
+	$trad["application_condition_exec"]["aparameter_id"] = "القيمة المعلمة";
+	$trad["application_condition_exec"]["aparameter_value"] = "قيمة القيمة المعلمة";
+	$trad["application_condition_exec"]["aparameter_value_date"] = "تاريخ القيمة المعلمة";
+	$trad["application_condition_exec"]["success_ind"] = "نجاح الشرط";
---
+++ tr/trad_en_application_condition_exec.php
@@ -0,0 +1,18 @@
+<?php
+	$trad["application_condition_exec"]["step1"] = "Definition";
+
+	$trad["application_condition_exec"]["applicationconditionexec.single"] = "Application condition exec";
+	$trad["application_condition_exec"]["applicationconditionexec.new"] = "new";
+	$trad["application_condition_exec"]["application_condition_exec"] = "Application condition execs";
+
+	$trad["application_condition_exec"]["applicant_id"] = "applicant";
+	$trad["application_condition_exec"]["application_plan_id"] = "application plan";
+	$trad["application_condition_exec"]["adesire_id"] = "desire";
+	$trad["application_condition_exec"]["acondition_id"] = "condition";
+	$trad["application_condition_exec"]["field_value"] = "field value";
+	$trad["application_condition_exec"]["field_date"] = "field date";
+	$trad["application_condition_exec"]["condition_exec_date"] = "condition date";
+	$trad["application_condition_exec"]["aparameter_id"] = "parameter id";
+	$trad["application_condition_exec"]["aparameter_value"] = "parameter value";
+	$trad["application_condition_exec"]["aparameter_value_date"] = "parameter date";
+	$trad["application_condition_exec"]["success_ind"] = "success indicator";
