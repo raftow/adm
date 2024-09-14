b465807e9e52d85e1861bcd3fa1b0ba555393eca
mohammed ali maaouani <mohammed.m@bmeholding.com>
2024-09-14 14:02:59 +0300

    recreate institution table and create identity_type table

--- tr/trad_en_institution.php
+++ tr/trad_en_institution.php
@@ -1,14 +1,22 @@
 <?php
+	$trad["institution"]["step1"] = "Definition";
 
 	$trad["institution"]["institution.single"] = "Institution";
 	$trad["institution"]["institution.new"] = "new";
 	$trad["institution"]["institution"] = "Institutions";
 
-	$trad["institution"]["country_id"] = "Country Id";
 	$trad["institution"]["institution_code"] = "Institution Code";
-	$trad["institution"]["institution_name_ar"] = "Institution Arabic Name";
-	$trad["institution"]["institution_name_en"] = "Institution English Name";
+	$trad["institution"]["institution_name_ar"] = "Institution Name - Ar";
+	$trad["institution"]["institution_name_en"] = "Institution Name - Ar";
+	$trad["institution"]["sci_id"] = "Sci";
+	$trad["institution"]["country_id"] = "Country";
 	$trad["institution"]["map_location"] = "Map Location";
-	$trad["institution"]["website"] = "website";
-	$trad["institution"]["adress"] = "Adress";
-	$trad["institution"]["postal_code"] = "Postal Code";
+	$trad["institution"]["website"] = "API Url";
+	$trad["institution"]["logo_file_id"] = "Logo";
+	$trad["institution"]["background_file_id"] = "Background";
+	$trad["institution"]["facebook_profile_link"] = "Facebook";
+	$trad["institution"]["linkedin_profile_link"] = "Linkedin";
+	$trad["institution"]["youtube_profile_link"] = "Youtube";
+	$trad["institution"]["snapchat_profile_link"] = "Snapchat";
+	$trad["institution"]["twitter_profile_link"] = "Twitter";
+	$trad["institution"]["instagram_profile_link"] = "Instagram";
