4ad8d8a38ee2ce73dc8f24fce033bfc7fd7b3e6c
mohammed ali maaouani <mohammed.m@bmeholding.com>
2024-10-06 15:14:50 +0300

    add new field taining unit type and structure major path

---
+++ migrations/migration_00008.php
@@ -0,0 +1,7 @@
+<?php
+// medali 06/10/2024
+AfwDatabase::db_query("alter table c0adm.academic_level add   training_unit_type_mfk varchar(255) DEFAULT NULL  after print_admission_doc;");
+
+
+
+//        
