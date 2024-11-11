ec1198dcd57eb4257d1f0e0a85aa9a050e1a0da7
mohammed ali maaouani <mohammed.m@bmeholding.com>
2024-11-11 13:29:07 +0300

    push application model financial transaction

---
+++ tr/trad_ar_application_model_financial_transaction.php
@@ -0,0 +1,22 @@
+<?php
+class ApplicationModelFinancialTransactionArTranslator{
+
+    public static function initData()
+    {
+        $trad = [];
+
+	$trad["application_model_financial_transaction"]["applicationmodelfinancialtransaction.single"] = "رسم نموذج قبول";
+	$trad["application_model_financial_transaction"]["applicationmodelfinancialtransaction.new"] = "جديد ة";
+	$trad["application_model_financial_transaction"]["application_model_financial_transaction"] = "رسوم نماذج القبول";
+	$trad["application_model_financial_transaction"]["financial_transaction_id"] = "الحركة المالية";
+	$trad["application_model_financial_transaction"]["application_model_id"] = "نموذج القبول";
+	$trad["application_model_financial_transaction"]["amount"] = "المبلغ";
+	$trad["application_model_financial_transaction"]["process_enabled"] = "تفعيل معالجة الدفع";
+        return $trad;
+        }
+
+        public static function getInstance()
+	{
+		return new ApplicationModelFinancialTransaction();
+	}
+}
