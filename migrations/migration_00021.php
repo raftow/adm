142f8a7de986d4c48af308c697877137df59f719
unknown <rafik.towati@gmail.com>
2024-11-24 16:59:14 +0300

    idn to id

--- migrations/migration_00021.php
+++ migrations/migration_00021.php
@@ -2,3 +2,4 @@
 $server_db_prefix = AfwSession::config("db_prefix", "default_db_");
 
 AfwDatabase::db_query("ALTER TABLE ".$server_db_prefix."adm.`applicant_api_request` CHANGE `applicant_id` `applicant_id` BIGINT(20) NOT NULL");
+AfwDatabase::db_query("ALTER TABLE ".$server_db_prefix."adm.`applicant` CHANGE `idn` `idn` VARCHAR(32) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL");
