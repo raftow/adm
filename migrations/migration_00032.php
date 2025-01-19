fb398c6502e94ef9e444488a1732feb26d981ae0
raftow <rboubaker@tvtc.gov.sa>
2025-01-19 10:43:10 +0300

    fix migrations of rafik

--- migrations/migration_00032.php
+++ migrations/migration_00032.php
@@ -3,5 +3,5 @@ $server_db_prefix = AfwSession::config("db_prefix", "default_db_");
 AfwDatabase::db_query("ALTER TABLE ".$server_db_prefix."adm.`training_unit_type` add program_track_id int(11) DEFAULT NULL  AFTER training_unit_type_code;"); 
 
 // some previleges
-Migration::generateTablePrevileges($moduleId, 'program_track', 189, "+t", null);
+
 
