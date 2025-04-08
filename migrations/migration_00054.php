0bdbe896164b1e2bf859722ebc6f346cbe787c4d
Rafik Laptop2 BME <rafik.towati@gmail.com>
2025-04-08 10:50:27 +0300

    Admission & Farz settings and simulator/manager

--- migrations/migration_00054.php
+++ migrations/migration_00054.php
@@ -2,4 +2,6 @@
 
 $server_db_prefix = AfwSession::config("db_prefix", "default_db_");
 
+AfwDatabase::db_query("ALTER TABLE ".$server_db_prefix."adm.application_step change   step_code step_code varchar(3)  DEFAULT NULL  AFTER step_name_en;");
+
 AfwDatabase::db_query("ALTER TABLE ".$server_db_prefix."adm.`screen_model` DROP `screen_title`");
