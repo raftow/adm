ffafd3a6fdfa53c79c97ac8a406216e2dd374d57
Rafik Laptop2 BME <rafik.towati@gmail.com>
2025-05-14 22:26:14 +0300

    Admission & Farz settings and simulator/manager

--- models/application_step.php
+++ models/application_step.php
@@ -386,6 +386,10 @@
                                         if($suffix2 and $method2 and $theObj and ($theObj->id>0))
                                         {
                                                 $stepFieldsArr[$scrIndex][$field_code."_".$suffix2] = $theObj->$method2($field_name);    
+                                                if($debugg==$field_code) 
+                                                {
+                                                        $stepFieldsArr[$scrIndex][$field_code."_".$suffix2."_sql"] = $theObj->debugg_sql_for_loadmany;    
+                                                }
                                         }
 
 
