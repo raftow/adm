40aaf5dffbf9218f5b40cd32391ca055936d7563
Rafik Laptop2 BME <rafik.towati@gmail.com>
2025-05-20 07:11:21 +0300

    Admission & Farz settings and simulator/manager

--- models/educational_zone.php
+++ models/educational_zone.php
@@ -47,18 +47,7 @@ class EducationalZone extends AdmObject
                 if ($id) {
                         if ($id_replace == 0) {
                                 // FK part of me - not deletable 
-                                // adm.applicant_qualification-المنطقة التعليمية	educational_zone_id  حقل يفلتر به (required field)
-                                // require_once "../adm/applicant_qualification.php";
-                                $obj = new ApplicantQualification();
-                                $obj->where("educational_zone_id = '$id' and active='Y' ");
-                                $nbRecords = $obj->count();
-                                // check if there's no record that block the delete operation
-                                if ($nbRecords > 0) {
-                                        $this->deleteNotAllowedReason = "Used in some Applicant qualifications(s) as educational_zone_id";
-                                        return false;
-                                }
-                                // if there's no record that block the delete operation perform the delete of the other records linked with me and deletable
-                                if (!$simul) $obj->deleteWhere("educational_zone_id = '$id' and active='N'");
+                                
 
 
 
@@ -66,7 +55,14 @@ class EducationalZone extends AdmObject
 
 
                                 // FK not part of me - replaceable 
+                                // adm.applicant_qualification-المنطقة التعليمية	educational_zone_id  حقل يفلتر به (required field)
+                                // require_once "../adm/applicant_qualification.php";
+                                if (!$simul) {
+                                        // require_once "../adm/applicant_qualification.php";
+                                        ApplicantQualification::updateWhere(array('educational_zone_id' => $id_replace), "educational_zone_id='$id'");
+                                        // $this->execQuery("update ${server_db_prefix}adm.applicant_qualification set educational_zone_id='$id_replace' where educational_zone_id='$id' ");
 
+                                }
 
 
                                 // MFK
