8251f03178def5a9e88888e0881c667bcc83cbb8
Rafik Laptop2 BME <rafik.towati@gmail.com>
2025-05-21 03:48:27 +0300

    Admission & Farz settings and simulator/manager

--- models/applicant.php
+++ models/applicant.php
@@ -255,7 +255,7 @@ class Applicant extends AdmObject
                                 }
                                 else
                                 {
-                                        die("fields_updated = ".var_export($fields_updated,true));
+                                        // die("fields_updated = ".var_export($fields_updated,true));
                                 }
                         }       
                 }
