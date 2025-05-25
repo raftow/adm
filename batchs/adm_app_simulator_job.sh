2dabe6ef4ea46ce0f1a4fdb3bb9eb9203338bb62
Rafik Laptop2 BME <rafik.towati@gmail.com>
2025-05-25 17:56:29 +0300

    Admission & Farz settings and simulator/manager

--- batchs/adm_app_simulator_job.sh
+++ batchs/adm_app_simulator_job.sh
@@ -2,4 +2,7 @@
 
 DATE=$(date '+%Y-%m-%d-%H-%M-%S')
 
-php /var/www/adm/batchs/adm_simulator_job.php $DATE >  /var/www/adm/log/adm_simulator_job-$DATE.log
+php /var/www/adm/batchs/adm_simulator_job.php $DATE >  /var/www/adm/log/adm_simulator_job-$DATE.log &
+
+echo "to see log run : ";
+echo "tail -f /var/www/adm/log/adm_simulator_job-$DATE.log ";
