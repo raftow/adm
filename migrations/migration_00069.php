9198df09316f68d284ffcfea69c619f01917991a
Medali Maaouani <maaoini@gmail.com>
2025-05-26 09:50:40 +0300

    application_plan_id

--- migrations/migration_00069.php
+++ migrations/migration_00069.php
@@ -5,6 +5,8 @@ $server_db_prefix = AfwSession::config("db_prefix", "default_db_");
 try
 {
     AfwDatabase::db_query("ALTER TABLE ".$server_db_prefix."adm.applicant_file add   document_type_id int(11) DEFAULT NULL  AFTER doc_type_id;");
+    AfwDatabase::db_query("ALTER TABLE ".$server_db_prefix."adm.service_request add   application_plan_id int(11) NOT NULL  AFTER applicant_file_id;");
+
     
 }catch(Exception $e)
 {
