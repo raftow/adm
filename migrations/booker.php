a36bd25adda07a2d31c1dc1b290d513cfcc67d32
Rafik Laptop2 BME <rafik.towati@gmail.com>
2025-05-26 11:54:30 +0300

    Book a migration or seeder script numbers

--- migrations/booker.php
+++ migrations/booker.php
@@ -73,6 +73,7 @@ $migrations = [
     '00067' => ['by'=>'medali','title'=>'applicant_evaluation.need_update'],
     '00068' => ['by'=>'medali','title'=>'service item changes'],
     '00069' => ['by'=>'medali','title'=>'applicant_file.document_type_id + service_request.application_plan_id'],
+    '00070' => ['by'=>'rafik','title'=>'sorting screen needs'],
     
 
 ];
