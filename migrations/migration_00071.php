8263f17062f64ec6c57d15cbd001a5e867d108a1
Medali Maaouani <maaoini@gmail.com>
2025-05-27 08:57:06 +0300

    field default null

--- migrations/migration_00071.php
+++ migrations/migration_00071.php
@@ -5,9 +5,9 @@ $server_db_prefix = AfwSession::config("db_prefix", "default_db_");
 try
 {
 
-    AfwDatabase::db_query("ALTER TABLE ".$server_db_prefix."adm.service_request add   request_status_id int(11) NOT NULL  AFTER application_plan_id;");
-    AfwDatabase::db_query("ALTER TABLE ".$server_db_prefix."adm.service_request add   request_type_id int(11) NOT NULL  AFTER request_status_id;");
-    AfwDatabase::db_query("ALTER TABLE ".$server_db_prefix."adm.service_request add   status_comment text  NOT NULL  AFTER request_type_id;");
+    AfwDatabase::db_query("ALTER TABLE ".$server_db_prefix."adm.service_request add   request_status_id int(11) DEFAULT NULL  AFTER application_plan_id;");
+    AfwDatabase::db_query("ALTER TABLE ".$server_db_prefix."adm.service_request add   request_type_id int(11) DEFAULT NULL  AFTER request_status_id;");
+    AfwDatabase::db_query("ALTER TABLE ".$server_db_prefix."adm.service_request add   status_comment text  DEFAULT NULL  AFTER request_type_id;");
     AfwDatabase::db_query("ALTER TABLE ".$server_db_prefix."adm.service_request add   status_date datetime DEFAULT NULL  AFTER status_comment;");
     AfwDatabase::db_query("ALTER TABLE ".$server_db_prefix."adm.service_request add   applicant_id int(11) DEFAULT NULL  AFTER status_date;");
     
