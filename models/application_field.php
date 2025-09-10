f3eb9e06dc900e1361fba767c05d6cd4f8897cd5
rafiq boubaker <rafik.towati@gmail.com>
2025-09-10 17:36:07 +0300

    work on uoh deployment from Tunis

--- models/application_field.php
+++ models/application_field.php
@@ -28,6 +28,10 @@ class ApplicationField extends AdmObject
           // ex $field_name=qsearch , $col_struct = READONLY => $attribute_prop = QSEARCH
           if($col_struct == "READONLY")
           {
+               if($attribute=="qsearch")
+               {
+                    if(!$classField) throw new AfwRuntimeException("$attribute application field (for object=".var_export($this, true).") <br> has strange table-id = ($application_table_id)");
+               }     
                if(!$classField) return false;
                $structField = $classField::getDbStructure($return_type = 'structure',
                          $attribute,'all',null, null, $repare=false);
