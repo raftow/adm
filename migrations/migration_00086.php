20324781bc8b9093d4c66ff204c1d56c6121b4d2
Medali Maaouani <maaoini@gmail.com>
2025-11-04 12:42:44 +0300

    study plan link

--- migrations/migration_00086.php
+++ migrations/migration_00086.php
@@ -15,7 +15,7 @@ try
     AfwDatabase::db_query("ALTER TABLE ".$server_db_prefix."adm.academic_term add   hijri_last_date_upload_doc varchar(8) DEFAULT NULL  AFTER hijri_sorting_end_date;");
 
     AfwDatabase::db_query("ALTER TABLE ".$server_db_prefix."adm.academic_term add   hijri_last_date_tuitfee varchar(8) DEFAULT NULL  AFTER hijri_last_date_upload_doc;");
-    AfwDatabase::db_query("ALTER TABLE ".$server_db_prefix."adm.academic_program add   Study_plan int(11) DEFAULT NULL  AFTER program_file_id");
+    AfwDatabase::db_query("ALTER TABLE ".$server_db_prefix."adm.academic_program add   Study_plan varchar(200) DEFAULT NULL  AFTER program_file_id");
     AfwDatabase::db_query("ALTER TABLE ".$server_db_prefix."adm.application_model add   admission_guide int(11) DEFAULT NULL  AFTER training_hours;");
 }
 catch(Exception $e)
