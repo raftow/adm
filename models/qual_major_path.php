d83d91827ab8bc6288425185f91c0e2e87c2e4b5
raftow <rboubaker@tvtc.gov.sa>
2025-11-04 10:06:04 +0300

    work on adm-v2

--- models/qual_major_path.php
+++ models/qual_major_path.php
@@ -1,38 +1,78 @@
 <?php
-        class QualMajorPath extends AdmObject{
 
-                public static $DATABASE		= ""; 
-                public static $MODULE		    = "adm"; 
-                public static $TABLE			= "qual_major_path"; 
-                public static $DB_STRUCTURE = null;
-                // public static $copypast = true;
+class QualMajorPath extends AdmObject
+{
 
-                public function __construct(){
-                        parent::__construct("qual_major_path","id","adm");
-                        AdmQualMajorPathAfwStructure::initInstance($this);
-                        
-                }
+    public static $DATABASE = '';
 
-                public static function loadById($id)
-                {
-                        $obj = new QualMajorPath();
-                        
-                        if($obj->load($id))
-                        {
-                                return $obj;
-                        }
-                        else return null;
-                }
+    public static $MODULE = 'adm';
 
-                public function getDisplay($lang = 'ar')
-                {
-                        return $this->getDefaultDisplay($lang);
-                }
+    public static $TABLE = 'qual_major_path';
 
-                public function stepsAreOrdered()
-                {
-                        return false;
-                }
+    public static $DB_STRUCTURE = null;
+    // public static $copypast = true;
 
+    public function __construct()
+    {
+        parent::__construct('qual_major_path', 'id', 'adm');
+        AdmQualMajorPathAfwStructure::initInstance($this);
+
+    }
+
+    public static function loadById($id)
+    {
+        $obj = new QualMajorPath();
+
+        if ($obj->load($id)) {
+            return $obj;
+        } else {
+            return null;
         }
-?>
+
+    }
+
+    public function getDisplay($lang = 'ar')
+    {
+        return $this->getDefaultDisplay($lang);
+    }
+
+    public function stepsAreOrdered()
+    {
+        return false;
+    }
+
+    public function beforeDelete($id, $id_replace)
+    {
+        $server_db_prefix = AfwSession::config('db_prefix', 'nauss_');
+
+        if (! $id) {
+            $id    = $this->getId();
+            $simul = true;
+        } else {
+            $simul = false;
+        }
+
+        if ($id) {
+
+            if ($id_replace == 0) {
+                // FK part of me - not deletable
+
+                // FK part of me - deletable
+
+                // FK not part of me - replaceable
+
+                // MFK
+
+            } else {
+                // FK on me
+
+                // MFK
+
+            }
+
+            return true;
+        }
+
+    }
+
+}
--- struct/qual_major_path_afw_structure.php
+++ struct/qual_major_path_afw_structure.php
@@ -38,7 +38,7 @@
                                         'major_path_id' => array('IMPORTANT' => 'IN',  'SEARCH' => true, 'QSEARCH' => true, 'SHOW' => true,  'RETRIEVE' => true,  
                                                 'EDIT' => true,  'QEDIT' => true, 'SHOW-ADMIN' => true,  'EDIT-ADMIN' => true,  'UTF8' => false,  
                                                 'TYPE' => 'FK',  'ANSWER' => 'major_path',  'ANSMODULE' => 'adm',  'SIZE' => 40,  'DEFAUT' => 0,    
-                                                'DISPLAY' => true,  'STEP' => 1,  'RELATION' => 'OneToMany', 'MANDATORY' => true, 'READONLY'=>false, 'AUTOCOMPLETE' => false,
+                                                'DISPLAY' => true,  'STEP' => 1,  'RELATION' => 'OneToMany', 'MANDATORY' => true, 'READONLY'=>false, 'AUTOCOMPLETE' => true,
                                                 'DISPLAY-UGROUPS' => '',  'EDIT-UGROUPS' => '', 
                                                 'CSS' => 'width_pct_25','WHERE' => ' qualification_id=§qualification_id§',
                                                 'DEPENDENCIES' => ['qualification_id'],
