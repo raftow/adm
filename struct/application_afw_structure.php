a82f41418f0343817a0269e70e1e8d1aeac101da
rafiq boubaker <rafik.towati@gmail.com>
2025-11-19 16:24:01 +0300

    work on nauss project

--- struct/application_afw_structure.php
+++ struct/application_afw_structure.php
@@ -343,7 +343,8 @@
                                                                 and gender_enum = §gender_enum§ 
                                                                 and (§training_period_enum§='0' or training_period_enum = §training_period_enum§)
                                                                 and (§consider_weighted_pctg§ != 'Y' or min_weighted_percentage <= §weighted_pctg§) 
-                                                                and program_offering_id in (0§program_offering_mfk§0)",
+                                                                and program_offering_id in (0§program_offering_mfk§0)
+                                                                and (application_end_date is null or application_end_date > now())",
                                                 'DNA' => true, 
                                                 'CSS' => 'width_pct_100', ),                                                                
 
