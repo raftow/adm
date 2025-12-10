d9d80bec0bc9ac6559263adcd98d68b6ac6d4d03
Medali Maaouani <maaoini@gmail.com>
2025-12-10 08:21:15 +0300

    add interview indicator

---
+++ migrations/migration_00096.php
@@ -0,0 +1,15 @@
+<?php
+if(!class_exists("AfwSession")) die("Denied access");
+
+$server_db_prefix = AfwSession::currentDBPrefix();
+try
+{
+           
+            //AfwDatabase::db_query("ALTER TABLE ".$server_db_prefix."adm.academic_program add   interview_ind char(1) NOT NULL DEFAULT 'W'  AFTER 	program_tuition_fees;");
+            AfwDatabase::db_query("ALTER TABLE ".$server_db_prefix."workflow.workflow_stage add   interview_ind char(1) NOT NULL DEFAULT 'W'  AFTER workflow_role_id;");
+            
+}
+catch(Exception $e)
+{
+    $migration_error .= " " . $e->getMessage();
+}    
