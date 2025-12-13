ca6c60ce0b46ffabd5df61d4636800957f11ece0
rafiq boubaker <rafik.towati@gmail.com>
2025-12-13 19:45:42 +0300

    work on nauss project

--- struct/application_model_financial_transaction_afw_structure.php
+++ struct/application_model_financial_transaction_afw_structure.php
@@ -6,9 +6,9 @@ class AdmApplicationModelFinancialTransactionAfwStructure
         {
                 if ($obj instanceof ApplicationModelFinancialTransaction) {
                         $obj->QEDIT_MODE_NEW_OBJECTS_DEFAULT_NUMBER = 3;
-                        $obj->DISPLAY_FIELD = "id";
+                        // $obj->DISPLAY_FIELD = "id";
                         // $obj->ORDER_BY_FIELDS = "xxxx, yyyy";
-                        $obj->UNIQUE_KEY = array('financial_transaction_id','application_model_id');
+                        $obj->UNIQUE_KEY = array('application_model_id', 'financial_transaction_id');
                         // $obj->public_display = true;
                         // $obj->IS_LOOKUP = true;
 
