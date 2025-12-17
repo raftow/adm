09a77c9b990a0b2c483a6d46afda747c884157f2
Medali Maaouani <maaoini@gmail.com>
2025-12-17 10:04:12 +0300

    beforeDelete

--- models/application_model_engagement.php
+++ models/application_model_engagement.php
@@ -34,5 +34,48 @@
                 {
                         return false;
                 }
+                public function beforeDelete($id,$id_replace) 
+        {
+            $server_db_prefix = AfwSession::config("db_prefix","nauss_");
+            
+            if(!$id)
+            {
+                $id = $this->getId();
+                $simul = true;
+            }
+            else
+            {
+                $simul = false;
+            }
+            
+            if($id)
+            {   
+               if($id_replace==0)
+               {
+                   // FK part of me - not deletable 
+
+                        
+                   // FK part of me - deletable 
+
+                   
+                   // FK not part of me - replaceable 
+
+                        
+                   
+                   // MFK
+
+               }
+               else
+               {
+                        // FK on me 
+
+                        
+                        // MFK
+
+                   
+               } 
+               return true;
+            }    
+	}
 
         }
