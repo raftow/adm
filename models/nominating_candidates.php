cd91f23a44b8d662d60233b28c72b7f925986ea8
rafiq boubaker <rafik.towati@gmail.com>
2025-12-19 08:51:12 +0300

    work on nauss project

--- models/nominating_candidates.php
+++ models/nominating_candidates.php
@@ -747,16 +747,18 @@ class NominatingCandidates extends AdmObject{
     public function calcAssignedProgramDiv($what = "value")
     {
         $lang = AfwLanguageHelper::getGlobalLanguage();
+        $message_war = $this->translateMessage('only one application branch is allowed', $lang);
+        $message_war_div = "<div class='info alert help warning'>$message_war</div>";
         if($this->getVal("academic_program_id")>0)
         {
             $message = $this->tm("Assigned academic program", $lang) . " : " . $this->decode("academic_program_id", '', false, $lang);
             $help_message = $this->tm("If this assigned program doesn't appear in the list below, you may need to do the overpass of the track condition (using the button here below this screen if you have the needed roles) or the academic program itself is not offered for this gender and scolar period", $lang);
             $help = "<div class='help'>$help_message</div>"; 
-            return "<div class='info alert'>$message $help</div>";
+            return "<div class='info alert'>$message $help</div>".$message_war_div;
         }
         else
         {
-            return "";
+            return $message_war_div;
         }
     }
 
