5f8343212eb2fc2b8d2d359d30ebf50ba451187d
rafiq boubaker <rafik.towati@gmail.com>
2026-01-05 10:49:04 +0300

    work on nauss project

--- migrations/migration_00094.php
+++ migrations/migration_00094.php
@@ -1,14 +1,12 @@
 <?php
-if(!class_exists("AfwSession")) die("Denied access");
+if (!class_exists('AfwSession'))
+    die('Denied access');
 
 $server_db_prefix = AfwSession::currentDBPrefix();
-try
-{
-           AfwDatabase::db_query("ALTER TABLE ".$server_db_prefix."adm.nominating_candidates add  country_id int(11) DEFAULT NULL  AFTER Mobile;");
-        
-    
+try {
+    // 183البرامج الدراسية
+    $migration_info .= ' ' . Atable::generateTablePrevileges($moduleId, 'program_requirement', 183, '+t', 'qsearch', null);
+    AfwDatabase::db_query('ALTER TABLE ' . $server_db_prefix . 'adm.nominating_candidates add  country_id int(11) DEFAULT NULL  AFTER Mobile;');
+} catch (Exception $e) {
+    $migration_error .= ' ' . $e->getMessage();
 }
-catch(Exception $e)
-{
-    $migration_error .= " " . $e->getMessage();
-}    
