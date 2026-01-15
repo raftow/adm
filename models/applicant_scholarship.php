d41e6aad6b5bc066f3a2ba4ed13766071f976763
Medali Maaouani <maaoini@gmail.com>
2026-01-15 15:53:14 +0300

    update candidates after maj

--- models/applicant_scholarship.php
+++ models/applicant_scholarship.php
@@ -78,6 +78,34 @@
                return true;
             }    
 	}
-
+        public function afterMaj($id, $fields_updated)
+        {
+                $app_id = $this->getVal("applicant_id");
+                if($app_id)
+                {
+                        $nominatingCandidateObj = new NominatingCandidates();
+                        $nominatingCandidateObj->where("applicant_id = '".$app_id."' and active = 'Y'");
+                        $nominatingCandidateObj->load();
+                        if($nominatingCandidateObj->id )
+                        {
+                                $nominationLetterObj = $nominatingCandidateObj->het("nomination_letter_id");
+                                $ScholarshipObj = $this->het("scholarship_id");
+                                $sponsor_id = $ScholarshipObj->getVal("sponsor_id");
+                                $applicationPlanObj = $nominationLetterObj->het("application_plan_id");
+                                //die($nominationLetterObj->getVal("application_plan_id")."++++");
+                                if($applicationPlanObj)
+                                {
+                                        //die( $applicationPlanObj->getVal("term_id")."----------");
+                                        if($sponsor_id==1 && $applicationPlanObj->getVal("term_id") == $ScholarshipObj->getVal("academic_term_id"))
+                                        {
+                                                $nominatingCandidateObj->set("study_funding_status_id", 3); // منحة الامير نايف
+                                                $nominatingCandidateObj->commit();
+                                        }
+                                }
+                                
+                                
+                        }
+                }
+        }
 }
 ?>
