cadaa784d856a8d78b27b3fb45950c378342c8dd
raftow <rboubaker@tvtc.gov.sa>
2026-04-22 10:28:04 +0300

    work on workflow - etl - crm - store - projects

---
+++ previleges/role/previleges_adm_role405_special.php
@@ -0,0 +1,11 @@
+<?php
+$role_info[405]['menu']['items'][10000000009] = array(
+    'id' => '10000000009',
+    'code' => 'execution',
+    'level' => '100',
+    'menu_name_ar' => 'الربط مع نظام معلومات الطلاب',
+    'menu_name_en' => 'Linking with the student information system',
+    'page' => 'main.php?Main_Page=sendDesireToSIS.php',
+    'css' => 'bf',
+    'icon' => 'bficon-std bfc-',
+);
