a5a09524183d4f35a237fbec5d4d3b4ac393e566
raftow <rboubaker@tvtc.gov.sa>
2026-05-02 21:26:42 +0300

    work on workflow - etl - crm - store - projects

--- login.php
+++ login.php
@@ -1,26 +1,3 @@
 <?php
-      $file_dir_name = dirname(__FILE__); 
-      include_once ("$file_dir_name/ini.php");
-      include_once ("$file_dir_name/module_config.php"); 
-      $direct_page = "login_ums.php";
-      $direct_page_path = "$file_dir_name/../ums";
-      // require("$file_dir_name/../lib/afw/cms/cms_main_page.php"); 
-      CmsMainPage::echoDirectPage($MODULE, $direct_page, $direct_page_path);
-
-      /**WSO2 */
-     /* $clientId = 'h51_DG3tGSlVV7tnmjNMuofMxAUa';
-$redirectUri = 'https://localhost/adm/login_ums.php'; // عدلها حسب دومينك الحقيقي
-
-$wso2AuthUrl = 'https://ethostest.nauss.edu.sa:443/oauth2/authorize';
-
-$params = [
-    'response_type' => 'code',
-    'client_id'     => $clientId,
-    'redirect_uri'  => $redirectUri,
-    'scope'         => 'openid profile email',
-    'state'         => bin2hex(random_bytes(8)), // لمنع CSRF
-];
-
-header('Location: ' . $wso2AuthUrl . '?' . http_build_query($params));
-exit;*/
-?>
+   include("adm_start.php");
+   include("$file_dir_name/../ums/login.php");
