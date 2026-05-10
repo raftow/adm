aee2002d50748fb17518e80e974dabf86a3bd606
rafiq boubaker <rafik.towati@gmail.com>
2026-05-10 17:56:47 +0300

    work on nauss project

---
+++ previleges/role/previleges_adm_role406.php
@@ -0,0 +1,218 @@
+<?php
+
+$role_info[406] = array(
+    'code' => 'goal-scholarship',
+    'name' =>
+    array(
+        'ar' => 'المنح الدراسية',
+        'en' => 'scholarship',
+    ),
+    'menu' =>
+    array(
+        'need_admin' => false,
+        'id' => '406',
+        'menu_name_' => 'المنح الدراسية',
+        'menu_name_ar' => 'المنح الدراسية',
+        'menu_name_en' => 'scholarship',
+        'page' => 'main.php?Main_Page=fm.php&a=1282&r=406',
+        'css' => 'info',
+        'icon' => ' icon-406',
+        'showme' => true,
+        'items' =>
+        array(
+            104825 =>
+            array(
+                'id' => '104825',
+                'code' => 'f2-a-scholarship/qsearch',
+                'level' => '999',
+                'menu_name_ar' => 'منح',
+                'menu_name_en' => 'Scholarships',
+                'page' => 'main.php?Main_Page=afw_mode_qsearch.php&cl=Scholarship&currmod=adm',
+                'css' => 'bf',
+                'icon' => 'bficon-104825 bfc-',
+            ),
+            105284 =>
+            array(
+                'id' => '105284',
+                'code' => 'f2-a-applicant_scholarship/qsearch',
+                'level' => '999',
+                'menu_name_ar' => 'منح المتقدمين',
+                'menu_name_en' => 'Applicant scholarships',
+                'page' => 'main.php?Main_Page=afw_mode_qsearch.php&cl=ApplicantScholarship&currmod=adm',
+                'css' => 'bf',
+                'icon' => 'bficon-105284 bfc-',
+            ),
+        ),
+        'otherbfs' =>
+        array(
+            104820 =>
+            array(
+                'id' => '104820',
+                'code' => 'f2-a-scholarship/edit',
+                'level' => '100',
+                'menu_name_ar' => 'إنشاء منحة',
+                'menu_name_en' => 'create Scholarship',
+                'page' => 'main.php?Main_Page=afw_mode_edit.php&cl=Scholarship&currmod=adm',
+                'css' => 'bf',
+                'icon' => 'bficon-104820 bfc-',
+            ),
+            104822 =>
+            array(
+                'id' => '104822',
+                'code' => 'f2-a-scholarship/delete',
+                'level' => '100',
+                'menu_name_ar' => 'مسح منحة',
+                'menu_name_en' => 'delete Scholarship',
+                'page' => 'main.php?Main_Page=afw_mode_delete.php&cl=Scholarship&currmod=adm',
+                'css' => 'bf',
+                'icon' => 'bficon-104822 bfc-',
+            ),
+            104823 =>
+            array(
+                'id' => '104823',
+                'code' => 'f2-a-scholarship/display',
+                'level' => '999',
+                'menu_name_ar' => 'عرض تفاصيل منحة',
+                'menu_name_en' => 'display details of Scholarship',
+                'page' => 'main.php?Main_Page=afw_mode_display.php&cl=Scholarship&currmod=adm',
+                'css' => 'bf',
+                'icon' => 'bficon-104823 bfc-',
+            ),
+            104824 =>
+            array(
+                'id' => '104824',
+                'code' => 'f2-a-scholarship/search',
+                'level' => '100',
+                'menu_name_ar' => 'البحث في منح',
+                'menu_name_en' => 'Scholarships search',
+                'page' => 'main.php?Main_Page=afw_mode_search.php&cl=Scholarship&currmod=adm',
+                'css' => 'bf',
+                'icon' => 'bficon-104824 bfc-',
+            ),
+            104825 =>
+            array(
+                'id' => '104825',
+                'code' => 'f2-a-scholarship/qsearch',
+                'level' => '999',
+                'menu_name_ar' => 'منح',
+                'menu_name_en' => 'Scholarships',
+                'page' => 'main.php?Main_Page=afw_mode_qsearch.php&cl=Scholarship&currmod=adm',
+                'css' => 'bf',
+                'icon' => 'bficon-104825 bfc-',
+            ),
+            105279 =>
+            array(
+                'id' => '105279',
+                'code' => 'f2-a-applicant_scholarship/edit',
+                'level' => '100',
+                'menu_name_ar' => 'إنشاء منحة متقدم',
+                'menu_name_en' => 'create Applicant scholarship',
+                'page' => 'main.php?Main_Page=afw_mode_edit.php&cl=ApplicantScholarship&currmod=adm',
+                'css' => 'bf',
+                'icon' => 'bficon-105279 bfc-',
+            ),
+            105281 =>
+            array(
+                'id' => '105281',
+                'code' => 'f2-a-applicant_scholarship/delete',
+                'level' => '100',
+                'menu_name_ar' => 'مسح منحة متقدم',
+                'menu_name_en' => 'delete Applicant scholarship',
+                'page' => 'main.php?Main_Page=afw_mode_delete.php&cl=ApplicantScholarship&currmod=adm',
+                'css' => 'bf',
+                'icon' => 'bficon-105281 bfc-',
+            ),
+            105282 =>
+            array(
+                'id' => '105282',
+                'code' => 'f2-a-applicant_scholarship/display',
+                'level' => '999',
+                'menu_name_ar' => 'عرض تفاصيل منحة متقدم',
+                'menu_name_en' => 'display details of Applicant scholarship',
+                'page' => 'main.php?Main_Page=afw_mode_display.php&cl=ApplicantScholarship&currmod=adm',
+                'css' => 'bf',
+                'icon' => 'bficon-105282 bfc-',
+            ),
+            105283 =>
+            array(
+                'id' => '105283',
+                'code' => 'f2-a-applicant_scholarship/search',
+                'level' => '100',
+                'menu_name_ar' => 'البحث في منح المتقدمين',
+                'menu_name_en' => 'Applicant scholarships search',
+                'page' => 'main.php?Main_Page=afw_mode_search.php&cl=ApplicantScholarship&currmod=adm',
+                'css' => 'bf',
+                'icon' => 'bficon-105283 bfc-',
+            ),
+            105284 =>
+            array(
+                'id' => '105284',
+                'code' => 'f2-a-applicant_scholarship/qsearch',
+                'level' => '999',
+                'menu_name_ar' => 'منح المتقدمين',
+                'menu_name_en' => 'Applicant scholarships',
+                'page' => 'main.php?Main_Page=afw_mode_qsearch.php&cl=ApplicantScholarship&currmod=adm',
+                'css' => 'bf',
+                'icon' => 'bficon-105284 bfc-',
+            ),
+            105298 =>
+            array(
+                'id' => '105298',
+                'code' => 'f2-a-applicant_scholarship_status/edit',
+                'level' => '100',
+                'menu_name_ar' => 'إنشاء حالة منحة متقدم',
+                'menu_name_en' => 'create Applicant scholarship status',
+                'page' => 'main.php?Main_Page=afw_mode_edit.php&cl=ApplicantScholarshipStatus&currmod=adm',
+                'css' => 'bf',
+                'icon' => 'bficon-105298 bfc-',
+            ),
+            105300 =>
+            array(
+                'id' => '105300',
+                'code' => 'f2-a-applicant_scholarship_status/delete',
+                'level' => '100',
+                'menu_name_ar' => 'مسح حالة منحة متقدم',
+                'menu_name_en' => 'delete Applicant scholarship status',
+                'page' => 'main.php?Main_Page=afw_mode_delete.php&cl=ApplicantScholarshipStatus&currmod=adm',
+                'css' => 'bf',
+                'icon' => 'bficon-105300 bfc-',
+            ),
+            105301 =>
+            array(
+                'id' => '105301',
+                'code' => 'f2-a-applicant_scholarship_status/display',
+                'level' => '100',
+                'menu_name_ar' => 'عرض تفاصيل حالة منحة متقدم',
+                'menu_name_en' => 'display details of Applicant scholarship status',
+                'page' => 'main.php?Main_Page=afw_mode_display.php&cl=ApplicantScholarshipStatus&currmod=adm',
+                'css' => 'bf',
+                'icon' => 'bficon-105301 bfc-',
+            ),
+            105302 =>
+            array(
+                'id' => '105302',
+                'code' => 'f2-a-applicant_scholarship_status/search',
+                'level' => '100',
+                'menu_name_ar' => 'البحث في حالات منح المتقدمين',
+                'menu_name_en' => 'Applicant scholarship statuss search',
+                'page' => 'main.php?Main_Page=afw_mode_search.php&cl=ApplicantScholarshipStatus&currmod=adm',
+                'css' => 'bf',
+                'icon' => 'bficon-105302 bfc-',
+            ),
+            105303 =>
+            array(
+                'id' => '105303',
+                'code' => 'f2-a-applicant_scholarship_status/qsearch',
+                'level' => '100',
+                'menu_name_ar' => 'حالات منح المتقدمين',
+                'menu_name_en' => 'Applicant scholarship statuss',
+                'page' => 'main.php?Main_Page=afw_mode_qsearch.php&cl=ApplicantScholarshipStatus&currmod=adm',
+                'css' => 'bf',
+                'icon' => 'bficon-105303 bfc-',
+            ),
+        ),
+        'sub-folders' =>
+        array(),
+    ),
+);
+//include "previleges_workflow_role406_special.php";
