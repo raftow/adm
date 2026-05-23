107d58bc1af1a601942cd1996f9dbfd6728390db
rafiq boubaker <rafik.towati@gmail.com>
2026-05-23 10:50:48 +0300

    some important improvement

---
+++ previleges/table/previleges_adm_table_financial_transaction_sis_settings.php
@@ -0,0 +1,50 @@
+<?php
+
+$tbf_info['financial_transaction_sis_settings'] = array(
+  'id' => '14047',
+  'display' =>
+  array(
+    'id' => '105313',
+  ),
+  'search' =>
+  array(
+    'id' => '105314',
+  ),
+  'qsearch' =>
+  array(
+    'id' => '105315',
+  ),
+  'edit' =>
+  array(
+    'id' => '105310',
+  ),
+  'qedit' =>
+  array(
+    'id' => '105311',
+  ),
+  'crossed' =>
+  array(
+    'id' => -1,
+  ),
+  'stats' =>
+  array(
+    'id' => -1,
+  ),
+  'ddb' =>
+  array(
+    'id' => -1,
+  ),
+  'minibox' =>
+  array(
+    'id' => -1,
+  ),
+  'delete' =>
+  array(
+    'id' => '105312',
+  ),
+);
+
+
+$tab_info[14047] = array(
+  'name' => 'financial_transaction_sis_settings',
+);
