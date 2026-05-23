7f6381355c33defe52435c904b2d87c91999107f
rafiq boubaker <rafik.towati@gmail.com>
2026-05-23 10:49:19 +0300

    some important improvement

--- previleges/table/previleges_adm_table_financial_transaction.php
+++ previleges/table/previleges_adm_table_financial_transaction.php
@@ -1,51 +1,50 @@
 <?php
 
-	$tbf_info['financial_transaction'] = array (
+$tbf_info['financial_transaction'] = array(
   'id' => '13952',
-  'display' => 
-  array (
+  'display' =>
+  array(
+    'id' => '105307',
+  ),
+  'search' =>
+  array(
+    'id' => '105308',
+  ),
+  'qsearch' =>
+  array(
+    'id' => '105309',
+  ),
+  'edit' =>
+  array(
+    'id' => '105304',
+  ),
+  'qedit' =>
+  array(
+    'id' => '105305',
+  ),
+  'crossed' =>
+  array(
     'id' => -1,
   ),
-  'search' => 
-  array (
+  'stats' =>
+  array(
     'id' => -1,
   ),
-  'qsearch' => 
-  array (
+  'ddb' =>
+  array(
     'id' => -1,
   ),
-  'edit' => 
-  array (
+  'minibox' =>
+  array(
     'id' => -1,
   ),
-  'qedit' => 
-  array (
-    'id' => -1,
-  ),
-  'crossed' => 
-  array (
-    'id' => -1,
-  ),
-  'stats' => 
-  array (
-    'id' => -1,
-  ),
-  'ddb' => 
-  array (
-    'id' => -1,
-  ),
-  'minibox' => 
-  array (
-    'id' => -1,
-  ),
-  'delete' => 
-  array (
-    'id' => -1,
+  'delete' =>
+  array(
+    'id' => '105306',
   ),
 );
 
 
-	$tab_info[13952] = array (
+$tab_info[13952] = array(
   'name' => 'financial_transaction',
 );
-
