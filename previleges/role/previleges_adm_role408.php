8f10c832d2fcf7e545be73a71b64e190c9f74cca
rafiq boubaker <rafik.towati@gmail.com>
2026-05-23 10:04:14 +0300

    some important improvement

--- previleges/role/previleges_adm_role408.php
+++ previleges/role/previleges_adm_role408.php
@@ -1,14 +1,14 @@
 <?php
 
-	$role_info[408] = array (
+$role_info[408] = array(
   'code' => 'goal-fts',
-  'name' => 
-  array (
+  'name' =>
+  array(
     'ar' => 'الحركات المالية',
     'en' => 'Financial transactions',
   ),
-  'menu' => 
-  array (
+  'menu' =>
+  array(
     'need_admin' => false,
     'id' => '408',
     'menu_name_' => 'الحركات المالية',
@@ -18,24 +18,24 @@
     'css' => 'info',
     'icon' => ' icon-408',
     'showme' => true,
-    'items' => 
-    array (
-      105305 => 
-      array (
+    'items' =>
+    array(
+      105305 =>
+      array(
         'id' => '105305',
         'code' => 'f2-a-financial_transaction/qedit',
-        'level' => '1',
+        'level' => '100',
         'menu_name_ar' => 'إدارة الحركات المالية',
         'menu_name_en' => 'financial transactions administration',
         'page' => 'main.php?Main_Page=afw_mode_qedit.php&cl=FinancialTransaction&currmod=adm&ids=all&newo=3',
         'css' => 'bf',
         'icon' => 'bficon-105305 bfc-',
       ),
-      105315 => 
-      array (
+      105315 =>
+      array(
         'id' => '105315',
         'code' => 'f2-a-financial_transaction_sis_settings/qsearch',
-        'level' => '1',
+        'level' => '100',
         'menu_name_ar' => 'التحكم في ارسال القيود المحاسبية',
         'menu_name_en' => 'Financial transaction sis settings',
         'page' => 'main.php?Main_Page=afw_mode_qsearch.php&cl=FinancialTransactionSisSettings&currmod=adm',
@@ -43,134 +43,134 @@
         'icon' => 'bficon-105315 bfc-',
       ),
     ),
-    'otherbfs' => 
-    array (
-      105304 => 
-      array (
+    'otherbfs' =>
+    array(
+      105304 =>
+      array(
         'id' => '105304',
         'code' => 'f2-a-financial_transaction/edit',
-        'level' => '1',
+        'level' => '100',
         'menu_name_ar' => 'إنشاء حركة مالية',
         'menu_name_en' => 'create financial transaction',
         'page' => 'main.php?Main_Page=afw_mode_edit.php&cl=FinancialTransaction&currmod=adm',
         'css' => 'bf',
         'icon' => 'bficon-105304 bfc-',
       ),
-      105305 => 
-      array (
+      105305 =>
+      array(
         'id' => '105305',
         'code' => 'f2-a-financial_transaction/qedit',
-        'level' => '1',
+        'level' => '100',
         'menu_name_ar' => 'إدارة الحركات المالية',
         'menu_name_en' => 'financial transactions administration',
         'page' => 'main.php?Main_Page=afw_mode_qedit.php&cl=FinancialTransaction&currmod=adm&ids=all&newo=3',
         'css' => 'bf',
         'icon' => 'bficon-105305 bfc-',
       ),
-      105306 => 
-      array (
+      105306 =>
+      array(
         'id' => '105306',
         'code' => 'f2-a-financial_transaction/delete',
-        'level' => '1',
+        'level' => '100',
         'menu_name_ar' => 'مسح حركة مالية',
         'menu_name_en' => 'delete financial transaction',
         'page' => 'main.php?Main_Page=afw_mode_delete.php&cl=FinancialTransaction&currmod=adm',
         'css' => 'bf',
         'icon' => 'bficon-105306 bfc-',
       ),
-      105307 => 
-      array (
+      105307 =>
+      array(
         'id' => '105307',
         'code' => 'f2-a-financial_transaction/display',
-        'level' => '1',
+        'level' => '100',
         'menu_name_ar' => 'عرض تفاصيل حركة مالية',
         'menu_name_en' => 'display details of financial transaction',
         'page' => 'main.php?Main_Page=afw_mode_display.php&cl=FinancialTransaction&currmod=adm',
         'css' => 'bf',
         'icon' => 'bficon-105307 bfc-',
       ),
-      105308 => 
-      array (
+      105308 =>
+      array(
         'id' => '105308',
         'code' => 'f2-a-financial_transaction/search',
-        'level' => '1',
+        'level' => '100',
         'menu_name_ar' => 'البحث في الحركات المالية',
         'menu_name_en' => 'financial transactions search',
         'page' => 'main.php?Main_Page=afw_mode_search.php&cl=FinancialTransaction&currmod=adm',
         'css' => 'bf',
         'icon' => 'bficon-105308 bfc-',
       ),
-      105309 => 
-      array (
+      105309 =>
+      array(
         'id' => '105309',
         'code' => 'f2-a-financial_transaction/qsearch',
-        'level' => '1',
+        'level' => '100',
         'menu_name_ar' => 'الحركات المالية',
         'menu_name_en' => 'financial transactions',
         'page' => 'main.php?Main_Page=afw_mode_qsearch.php&cl=FinancialTransaction&currmod=adm',
         'css' => 'bf',
         'icon' => 'bficon-105309 bfc-',
       ),
-      105310 => 
-      array (
+      105310 =>
+      array(
         'id' => '105310',
         'code' => 'f2-a-financial_transaction_sis_settings/edit',
-        'level' => '1',
+        'level' => '100',
         'menu_name_ar' => 'إنشاء التحكم في ارسال القيود المحاسبية',
         'menu_name_en' => 'create Financial transaction sis settings',
         'page' => 'main.php?Main_Page=afw_mode_edit.php&cl=FinancialTransactionSisSettings&currmod=adm',
         'css' => 'bf',
         'icon' => 'bficon-105310 bfc-',
       ),
-      105311 => 
-      array (
+      105311 =>
+      array(
         'id' => '105311',
         'code' => 'f2-a-financial_transaction_sis_settings/qedit',
-        'level' => '1',
+        'level' => '100',
         'menu_name_ar' => 'إدارة التحكم في ارسال القيود المحاسبية',
         'menu_name_en' => 'Financial transaction sis settings administration',
         'page' => 'main.php?Main_Page=afw_mode_qedit.php&cl=FinancialTransactionSisSettings&currmod=adm&ids=all&newo=3',
         'css' => 'bf',
         'icon' => 'bficon-105311 bfc-',
       ),
-      105312 => 
-      array (
+      105312 =>
+      array(
         'id' => '105312',
         'code' => 'f2-a-financial_transaction_sis_settings/delete',
-        'level' => '1',
+        'level' => '100',
         'menu_name_ar' => 'مسح التحكم في ارسال القيود المحاسبية',
         'menu_name_en' => 'delete Financial transaction sis settings',
         'page' => 'main.php?Main_Page=afw_mode_delete.php&cl=FinancialTransactionSisSettings&currmod=adm',
         'css' => 'bf',
         'icon' => 'bficon-105312 bfc-',
       ),
-      105313 => 
-      array (
+      105313 =>
+      array(
         'id' => '105313',
         'code' => 'f2-a-financial_transaction_sis_settings/display',
-        'level' => '1',
+        'level' => '100',
         'menu_name_ar' => 'عرض تفاصيل التحكم في ارسال القيود المحاسبية',
         'menu_name_en' => 'display details of Financial transaction sis settings',
         'page' => 'main.php?Main_Page=afw_mode_display.php&cl=FinancialTransactionSisSettings&currmod=adm',
         'css' => 'bf',
         'icon' => 'bficon-105313 bfc-',
       ),
-      105314 => 
-      array (
+      105314 =>
+      array(
         'id' => '105314',
         'code' => 'f2-a-financial_transaction_sis_settings/search',
-        'level' => '1',
+        'level' => '100',
         'menu_name_ar' => 'البحث في التحكم في ارسال القيود المحاسبية',
         'menu_name_en' => 'Financial transaction sis settings search',
         'page' => 'main.php?Main_Page=afw_mode_search.php&cl=FinancialTransactionSisSettings&currmod=adm',
         'css' => 'bf',
         'icon' => 'bficon-105314 bfc-',
       ),
-      105315 => 
-      array (
+      105315 =>
+      array(
         'id' => '105315',
         'code' => 'f2-a-financial_transaction_sis_settings/qsearch',
-        'level' => '1',
+        'level' => '100',
         'menu_name_ar' => 'التحكم في ارسال القيود المحاسبية',
         'menu_name_en' => 'Financial transaction sis settings',
         'page' => 'main.php?Main_Page=afw_mode_qsearch.php&cl=FinancialTransactionSisSettings&currmod=adm',
@@ -178,9 +178,8 @@
         'icon' => 'bficon-105315 bfc-',
       ),
     ),
-    'sub-folders' => 
-    array (
-    ),
+    'sub-folders' =>
+    array(),
   ),
 );
-include "previleges_adm_role407_special.php";
+include "previleges_adm_role408_special.php";
