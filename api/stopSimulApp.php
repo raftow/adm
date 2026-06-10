e4ad55ca742550801e1bd41207beb7f0baf4b166
raftow <rboubaker@tvtc.gov.sa>
2026-06-10 11:31:15 +0300

    work on workflow - etl - crm - store - projects

--- api/stopSimulApp.php
+++ api/stopSimulApp.php
@@ -4,9 +4,6 @@ $file_dir_name = dirname(__FILE__);
 require_once("$file_dir_name/../../lib/afw/core/afw_autoloader.php");
 set_time_limit(8400);
 ini_set('error_reporting', E_ERROR | E_PARSE | E_RECOVERABLE_ERROR | E_CORE_ERROR | E_COMPILE_ERROR | E_USER_ERROR);
-$lang = "en";
-
-
         
 AfwSession::startSession();
 
@@ -16,7 +13,9 @@ $only_members = true;
 $debug_name = "autocomplete";
 require("$file_dir_name/../lib/afw/includes/afw_check_member.php");
 
-if(!$objme) $objme = AfwSession::getUserConnected();
+if(!isset($objme)) $objme = AfwSession::getUserConnected();
+if(!isset($lang)) $lang = AfwLanguageHelper::getGlobalLanguage();
+
  
 // 
 
